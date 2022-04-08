a5f3b02a23f6910e52ebaf07be3ffc05acd67232
adtimokhin <adtimokhin@gmail.com>
2022-04-08 16:15:49 +0300

    JJJ

---
+++ middlewares/is-auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const getJWTSecret = require("../utils/jwt-security.js").getJWTSecret;
+
+const throwInvalidJWTError = (statusCode = 500) => {
+  const error = new Error("Invalid JWT token.");
+  error.statusCode = statusCode;
+  error.message = "Invalid JWT token.";
+  throw error;
+};
+
+module.exports.isAuth = (req, res, next) => {
+  // This funnction checks the headers to find the 'Authorization' header. It will then determine whether the JWT passed is valid.
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    throwInvalidJWTError(401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    throwInvalidJWTError(401);
+  }
+
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, getJWTSecret());
+  } catch (err) {
+    throwInvalidJWTError();
+  }
+
+  if (!decodedToken) {
+    throwInvalidJWTError(401);
+  }
+  next();
+};
